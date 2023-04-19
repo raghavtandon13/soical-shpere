8c23a4bea0276c7649e8077c84600d3766e642e6
Raghav Tandon <121853010+raghavtandon13@users.noreply.github.com>
2023-04-20 01:01:26 +0530

    done

---
+++ server/routes/users.js
@@ -0,0 +1,18 @@
+import express from "express";
+import {
+    getUser,
+    getUserFriends,
+    addRemoveFrined,
+} from "../controllers/users.js";
+import { verifyToken } from "../middleware/auth.js";
+
+const router = express.Router();
+
+/* READ */
+router.get("/:id", verifyToken, getUser);
+router.get("/:id/friends", verifyToken, getUserFriends);
+
+/* UPDATE */
+router.patch("/:id/:friendId", verifyToken, addRemoveFrined);
+
+export default router;
